b6e1b431922b02333bda0e0cfd0352610d8e0505
Nokitaki <joelchandlerp@gmail.com>
2025-02-18 00:31:09 +0800

    Authentication

---
+++ oauth2web/src/main/java/com/pili/oauth2web/Oauth2webApplication.java
@@ -0,0 +1,13 @@
+package com.pili.oauth2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2webApplication.class, args);
+	}
+
+}
---
+++ oauth2web/src/test/java/com/pili/oauth2web/Oauth2webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pili.oauth2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
